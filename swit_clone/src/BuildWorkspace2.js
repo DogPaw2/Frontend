388475f0aec99c3a875b3e70e2050429837cc61c
dianestar <dianestar@naver.com>
2021-08-26 23:08:21 +0900

    Invitation toggle switch & alert modal COMPLETE

--- swit_clone/src/BuildWorkspace2.js
+++ swit_clone/src/BuildWorkspace2.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import SwitchButton from './SwitchButton';
+import Switch from 'react-switch';
+import LinkModal from './LinkModal';
+
 import './SwitHome.css';
 import './BuildWorkspace.css';
 import { propTypes } from 'react-bootstrap/esm/Image';
@@ -15,13 +17,32 @@ function BuildWorkspace() {
     }
     
     const [copied, setCopied] = useState(false);
+    const hiddenCopied = () => {
+        setCopied(false);
+    }
     const confirmCopied = () => {
-        setCopied(!copied);
+        setCopied(true);
+        setTimeout(hiddenCopied, 2000);
     }
 
     const [checked, setChecked] = useState(false);
-    const onCheckedChange = (checked) => {
-        setChecked(checked);
+    const onCheckedChange = () => {
+        setChecked(!checked);
+        if (checked == true) {
+            openModal();
+        }
+    }
+    const [modalOpen, setModalOpen] = useState(false);
+    const openModal = () => {
+        setModalOpen(true);
+    }
+    const cancelModal = () => {
+        setModalOpen(false);
+        setChecked(true);
+    }
+    const deleteModal = () => {
+        setModalOpen(false);
+        setChecked(false);
     }
 
     return (
@@ -52,17 +73,45 @@ function BuildWorkspace() {
                         <FontAwesomeIcon className="invitation-icon" icon={faLink}/>
                         <h4 className="invitation-title">Invite people via link</h4>
                         <div className="link-creation-div">
-                            <SwitchButton ischecked={checked} onChange={onCheckedChange}/>
+                            <Switch
+                                className="switch-button"
+                                checked={checked}
+                                onChange={onCheckedChange}
+                                offColor="#D8D8D8"
+                                onColor="#1D5ECC"
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                activeBoxShadow=""
+                                height={18}
+                                width={32}
+                                handleDiameter={16} />
                             <label className="link-invitation-direction">Activate invitation link</label>
                         </div>
-                        <input className="new-invitation-link" value="https://invite.swit.io/blahblah" onChange={onUrlChange}></input>
-                        <CopyToClipboard text={inviteUrl}>
-                            <button className="link-copy-button" onClick={confirmCopied}>Copy</button>
-                        </CopyToClipboard>
+                        {checked ?
+                        <div>
+                            <input className="new-invitation-link" value="https://invite.swit.io/blahblah" onChange={onUrlChange}></input>
+                            <CopyToClipboard text={inviteUrl}>
+                                <button className="link-copy-button" onClick={confirmCopied}>Copy</button>
+                            </CopyToClipboard>
+                        </div>
+                        : 
+                        <div>
+                            { modalOpen ? 
+                            <div>
+                                <input className="new-invitation-link" value="https://invite.swit.io/blahblah" onChange={onUrlChange}></input>
+                                <CopyToClipboard text={inviteUrl}>
+                                    <button className="link-copy-button" onClick={confirmCopied}>Copy</button>
+                                </CopyToClipboard>
+                                <LinkModal open={modalOpen} cancel={cancelModal} del={deleteModal}></LinkModal>
+                            </div>
+                            : null
+                            }
+                        </div>
+                        }
+
                     </div>
                     <span className="invite-later-span">Invite later</span>
-                    {copied ? <div className="copy-confirmed-div">Link copied to clipboard.</div> : <div></div>}
-
+                    {copied ? <div className="copy-confirmed-div">Link copied to clipboard.</div> : null}
                 </div>
             </div>
         </div>
---
+++ swit_clone/src/LinkModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./LinkModal.css";
+
+const LinkModal = ( props ) => {
+    const { open, cancel, del } = props;
+
+    return (
+        <div className={ open ? 'openModal modal' : 'modal' }>
+            { open ? (  
+                <div>
+                    <div className="modal-content">
+                        <h3>Delete invitation link</h3> 
+                        <p className="modal-direction-p">If you delete the invitation link, it will no longer be valid and people won't be able to join the workspace with this link.</p>
+                        <div className="modal-btn-div">
+                            <button className="modal-cancel-btn" onClick={cancel}>Cancel</button>
+                            <button className="modal-delete-btn" onClick={del}>Delete</button>
+                        </div>
+                    </div>
+                </div>
+            ) : null }
+        </div>
+    )
+}
+
+export default LinkModal;
